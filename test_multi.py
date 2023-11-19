3d5227609b00ff6ea9baba3a1e16c8301c901989
rgut96 <r.gutimer96@gmail.com>
2023-11-19 19:29:38 +0100

    otro error corregido

--- test_multi.py
+++ test_multi.py
@@ -6,7 +6,7 @@ class TestMulti(unittest.TestCase):
  def test_multi(self):
   self.assertEqual(multi(3, 2), 6)
   self.assertEqual(multi(-1, 1), -1)
-  self.assertEqual(multi(-1, -1), -1)
+  self.assertEqual(multi(-1, -1), 1)
 
 if __name__ == '__main__':
  unittest.main()
